b5f9f914d965c2dcabdc4caa56dbc9c8a5b39c39
nathielems <nathielems@users.noreply.github.com>
2020-06-21 15:15:26 -0300

    criação da classe evolution e measures

---
+++ Academia.Domain/Entities/Evolution.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Academia.Domain.Entities
+{
+    public class Evolution
+    {
+        public String Day { get; set; }
+
+        public String Weight { get; set; }
+
+        public String Height { get; set; }
+
+
+    }
+}
---
+++ Academia.Domain/Entities/Measures.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Academia.Domain.Entities
+{
+    public class Measures
+    {
+        public string Pulse { get; set; }
+
+
+
+    }
+}
